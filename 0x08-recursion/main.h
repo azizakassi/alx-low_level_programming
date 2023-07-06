9c6663e02b351eb74a623faf229649f560385c0e
azizakassi <azizakassi07@gmail.com>
2023-07-05 19:01:09 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char)
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int strien_recursion (char *s);
+int factorial(int n):
+int pox recursion (int x, int y):
+int sqrt_recursion (int n):
+int is prize_number(int n):
+int is palindrone (char $3);
+int wildcmp (char s1, char *s2);
+
+#endif 
